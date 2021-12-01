dbcee17ac35d8fedf7be951486cb8957aee295e6
Mahmuda <mahmudaakhtergithub@gmail.com>
2021-12-01 20:04:57 +0600

    rich

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(
+    MyApp()
+  );
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold (
+        backgroundColor: Colors.teal,
+        body: SafeArea (
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.only(left: 10.0),
+            padding: EdgeInsets.all(20),
+            color: Colors.red,
+            child: Text('Hello'),
+        ),
+      ),
+    ),
+  );
+ }
+}
+
